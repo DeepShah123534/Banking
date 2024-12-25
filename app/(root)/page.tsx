db7358ae97af8479f564a83fffb2be1781fc87e1
DeepShah123534 <deepshah0206@gmail.com>
2024-12-25 00:01:24 -0500

    1 hour done

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,8 +1,31 @@
-import React from 'react'
+
+import TotalBalanceBox from '@/components/TotalBalanceBox'
+import HeaderBox from '@/components/ui/HeaderBox'
+
 
 const Home = () => {
+  const loggedIn = { firstName: 'Deep' }
   return (
-    <div>Home</div>
+    <section className = "home">
+      <div className = "home-content">
+        <header className = "home-header">
+          <HeaderBox
+           type ="greeting"
+           title="Welcome"
+           user={loggedIn?.firstName || 'Guest'}
+           subtext="Access and manage you account and transactions efficiently."
+          />
+          <TotalBalanceBox 
+           accounts = {[]}
+           totalBanks={1}
+           totalCurrentBalance = {1250.35}
+           
+          />
+        </header>
+
+
+      </div>
+    </section>
   )
 }
 
